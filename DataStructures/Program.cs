afc5ed3c59e095d5eede89741ac56dd17d1290dd
markbennison <mark.bennison@live.co.uk>
2024-09-02 16:35:53 +0100

    Added 'someFloat' to output to 3dp.

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -6,6 +6,11 @@
         {
             Console.WriteLine("Hello, World!");
             Console.ReadKey();
+
+            float someFloat = 3.14159f;
+            Console.WriteLine(someFloat.ToString("0.000"));
+            Console.ReadKey();
+
         }
     }
 }
