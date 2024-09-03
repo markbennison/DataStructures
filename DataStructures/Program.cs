b115fdd9d07947bd2f5fb200511d9f4148c97cdc
markbennison <mark.bennison@live.co.uk>
2024-09-03 17:00:44 +0100

    Movie Structure and MovieRating Enum

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -4,13 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.ReadKey();
+            Movie Deadpool3;
+            Deadpool3.title = "Deadpool & Wolverine";
+            Deadpool3.releaseYear = 2024;
+            Deadpool3.price = 19.99;
+            Deadpool3.rating = MovieRating._15;
 
-            float someFloat = 3.14159f;
-            Console.WriteLine(someFloat.ToString("0.000"));
+            Console.WriteLine(Deadpool3.title + " (" + Deadpool3.releaseYear + ") Rated " + Deadpool3.rating);
+
             Console.ReadKey();
 
         }
+
+        public enum MovieRating
+        {
+            _U,
+            _12A,
+            _12,
+            _15,
+            _18
+        }
+
+        public struct Movie
+        {
+            public string title;
+            public int releaseYear;
+            public double price;
+            public MovieRating rating;
+
+        }
+
     }
 }
